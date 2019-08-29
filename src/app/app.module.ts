c7ab93d29bbb7c95a4bcfa711a8fe7d7a61baa20
Daniel Fernandez Ruiz <dfernandez@airzonesl.es>
2019-08-29 13:57:41 +0200

    Lo partimos en header, sidebar, principal y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/configs/config.ts
@@ -0,0 +1,3 @@
+export const datosHeader = {
+    titulo: 'Airzone Project'
+}
