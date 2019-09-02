26b2ae6236d2e7cc92ddec62b28e84928409e484
Daniel Fernandez Ruiz <dfernandez@airzonesl.es>
2019-09-02 16:22:03 +0200

    Listado de proyectos y estadísticas

--- src/app/components/pages/statistics/statistics.component.ts
+++ src/app/components/pages/statistics/statistics.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+
+declare function initChart();
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styles: []
 })
-export class StatisticsComponent implements OnInit {
+
+
+export class StatisticsComponent implements OnInit, AfterContentChecked {
 
   constructor() { }
 
   ngOnInit() {
+    initChart();
+  }
+
+  ngAfterContentChecked () {
+    
   }
 
 }
